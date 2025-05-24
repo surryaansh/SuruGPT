254d4d995c2c4681b602172a057b55bf5b4b00a2
surryaansh <suryanshs1804@gmail.com>
2025-05-25 00:17:27 +0530

    Update firebaseConfig.ts

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,1 +1,10 @@
-
+// services/firebaseConfig.ts
+export const firebaseConfig = {
+  apiKey: "AIzaSyAIVE8hNP_Amcc_jt7kioHiv51_q6NFHNU",
+  authDomain: "suru-3188c.firebaseapp.com",
+  projectId: "suru-3188c",
+  storageBucket: "suru-3188c.firebasestorage.app",
+  messagingSenderId: "924168146042",
+  appId: "1:924168146042:web:2e65a43a39cec78fa67fc3",
+  measurementId: "G-CJJCV4H5DZ"
+};
