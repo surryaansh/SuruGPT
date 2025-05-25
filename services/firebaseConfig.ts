71ce330db2f8a010c1d8e9c4fcbde44e748f58a7
surryaansh <suryanshs1804@gmail.com>
2025-05-25 13:27:51 +0530

    Update firebaseConfig.ts

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -8,3 +8,7 @@ export const firebaseConfig = {
   appId: "1:924168146042:web:2e65a43a39cec78fa67fc3", // Example value, use YOURS
   measurementId: "G-CJJCV4H5DZ"                      // Example value, use YOURS (if provided)
 };
+// Check if the configuration is still using placeholder values.
+export const IS_FIREBASE_CONFIG_PLACEHOLDER = 
+  firebaseConfig.apiKey === "AIzaSyAIVE8hNP_Amcc_jt7kioHiv51_q6NFHNU" || 
+  firebaseConfig.projectId === "suru-3188c";
