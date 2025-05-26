af30db7cf6d8362a9f23235ab224e3e5c1b57a32
surryaansh <suryanshs1804@gmail.com>
2025-05-26 23:07:02 +0530

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -78,30 +78,56 @@ export const IconKawaiiSuru: React.FC<{ className?: string }> = ({ className = "
 
 // FIX: Added style prop to IconSuru to allow inline styles.
 export const IconSuru: React.FC<{ className?: string, style?: React.CSSProperties }> = ({ className = "w-6 h-6 text-[#FF8DC7]", style }) => (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className} style={style}>
-        <path d="M7 13C7 14.1046 7.89543 15 9 15C10.1046 15 11 14.1046 11 13C11 11.8954 10.1046 11 9 11C7.89543 11 7 11.8954 7 13Z" fill="#EAE6F0"/>
-        <path d="M13 13C13 14.1046 13.8954 15 15 15C16.1046 15 17 14.1046 17 13C17 11.8954 16.1046 11 15 11C13.8954 11 13 11.8954 13 13Z" fill="#EAE6F0"/>
-        <path fillRule="evenodd" clipRule="evenodd" d="M5 7C5 5.34315 6.34315 4 8 4H16C17.6569 4 19 5.34315 19 7V17C19 18.6569 17.6569 20 16 20H8C6.34315 20 5 18.6569 5 17V7ZM8 6C7.44772 6 7 6.44772 7 7V17C7 17.5523 7.44772 18 8 18H16C17.5523 18 18 17.5523 18 17V7C18 6.44772 17.5523 6 16 6H8Z" fill="currentColor"/>
-        <path d="M9 16.5H15" stroke="#EAE6F0" strokeWidth="1.5" strokeLinecap="round"/>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={className} style={style} aria-hidden="true">
+        {/* Main circle for face */}
+        <circle cx="12" cy="12" r="10" fill="currentColor" />
+        {/* Eyes */}
+        <circle cx="9" cy="10" r="1.5" fill="#EAE6F0" />
+        <circle cx="15" cy="10" r="1.5" fill="#EAE6F0" />
+        {/* Smile */}
+        <path d="M8 14 Q12 16.5 16 14" stroke="#EAE6F0" strokeWidth="1.2" strokeLinecap="round" fill="none" />
+         {/* Small blush marks - optional, can be removed if too detailed */}
+        <ellipse cx="7" cy="12.5" rx="1.2" ry="0.8" fill="#FFB6C1" opacity="0.5" />
+        <ellipse cx="17" cy="12.5" rx="1.2" ry="0.8" fill="#FFB6C1" opacity="0.5" />
     </svg>
 );
 
-export const IconUser: React.FC<{ className?: string }> = ({ className = "w-6 h-6 text-[#EAE6F0]" }) => (
+
+export const IconHeart: React.FC<{ className?: string, style?: React.CSSProperties }> = ({ className = "w-6 h-6", style }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className} style={style} aria-hidden="true">
+      <path d="M11.645 20.91a.75.75 0 0 1-1.29 0L1.759 10.336a4.5 4.5 0 0 1 6.364-6.364l.69.69.69-.69a4.5 4.5 0 0 1 6.364 6.364L11.645 20.91Z" />
+    </svg>
+  );
+
+export const IconThumbUp: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H5.904c-.656 0-1.233-.425-1.435-1.026a4.782 4.782 0 0 1 0-2.944C4.671 3.425 5.248 3 5.904 3H15.31c.656 0 1.233.425 1.435 1.026a4.782 4.782 0 0 1 0 2.944c-.202.601-.779 1.026-1.435 1.026Z" />
+  </svg>
+);
+export const IconThumbUpSolid: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
-    <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
+    <path d="M7.493 18.75c-.425 0-.82-.236-.975-.632a1.5 1.5 0 0 1 .522-1.666l.983-.805A9.004 9.004 0 0 1 9 15.082V9.75a3 3 0 0 1 3-3h.375c.621 0 1.2.201 1.62.562l.668.557c.25.208.532.39.832.537.299.145.617.228.945.228h3.125c.621 0 1.125.504 1.125 1.125V15c0 .621-.504 1.125-1.125 1.125h-4.372c.123.47.175.965.175 1.471 0 .624-.134 1.22-.39 1.766a2.99 2.99 0 0 1-1.164 1.123c-.344.196-.711.291-1.078.291H9.375a3 3 0 0 1-1.882-.666Z" />
+    <path d="M5.25 7.5c0-.828-.672-1.5-1.5-1.5h-1.5a1.5 1.5 0 0 0-1.5 1.5v11.25c0 .828.672 1.5 1.5 1.5h1.5a1.5 1.5 0 0 0 1.5-1.5V7.5Z" />
   </svg>
 );
 
-export const IconHeart: React.FC<{ className?: string, style?: React.CSSProperties }> = ({ className = "w-6 h-6 text-[#FF8DC7]", style }) => (
-  <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    viewBox="0 0 24 24"
-    fill="currentColor" 
-    className={className}
-    style={style}
-    aria-hidden="true"
-  >
-    <path fillRule="evenodd" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" clipRule="evenodd" />
+
+export const IconThumbDown: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.25c.806 0 1.533.446 2.031 1.08a9.041 9.041 0 0 1 2.861 2.4c.723.384 1.35.956 1.653 1.715a4.498 4.498 0 0 0 .322 1.672V17.25a.75.75 0 0 1-.75.75 2.25 2.25 0 0 1-2.25-2.25c0-1.152.26-2.243.723-3.218.266-.558-.107-1.282-.725-1.282m0 0H9.375c-1.026 0-1.945-.694-2.054-1.715A9.965 9.965 0 0 1 7.248 6.5c-.388-.482-.987-.729-1.605-.729H4.158c-.483 0-.964.078-1.423.23L1.62 7.051a4.501 4.501 0 0 0-1.423.23H0M13.367 3.75H5.904c-.656 0-1.233.425-1.435 1.026a4.782 4.782 0 0 0 0 2.944c.202.601.779 1.026 1.435 1.026H13.367c.656 0 1.233-.425 1.435-1.026a4.782 4.782 0 0 0 0-2.944C14.6 4.175 14.023 3.75 13.367 3.75Z" />
+  </svg>
+);
+export const IconThumbDownSolid: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
+    <path d="M11.25 5.25c0-.828.672-1.5 1.5-1.5h1.5a1.5 1.5 0 0 1 1.5 1.5v11.25c0 .828-.672 1.5-1.5 1.5h-1.5a1.5 1.5 0 0 1-1.5-1.5V5.25Z" />
+    <path d="M6 11.25c.123-.47.175-.965.175-1.471 0-.624-.134-1.22-.39-1.766a2.99 2.99 0 0 0-1.164-1.123c-.344-.196-.711-.291-1.078-.291H2.625a3 3 0 0 0-1.882.666C.305 7.785.025 8.237 0 8.718c0 .425.22.82.621 1.076a1.5 1.5 0 0 0 1.657.101l.983-.805A9.004 9.004 0 0 0 6 8.918v5.332a3 3 0 0 0 3 3h.375c.621 0 1.2-.201 1.62-.562l.668-.557c.25-.208.532-.39.832-.537.299-.145.617-.228.945-.228H15c.621 0 1.125-.504 1.125-1.125V9a1.125 1.125 0 0 0-1.125-1.125h-4.372Z" />
+  </svg>
+);
+
+
+export const IconArrowPath: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 11.667 0l3.181-3.183m0-4.991v4.99" />
   </svg>
 );
 
@@ -113,63 +139,12 @@ export const IconEllipsisVertical: React.FC<{ className?: string }> = ({ classNa
 
 export const IconTrash: React.FC<{ className?: string }> = ({ className = "w-5 h-5" }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12.56 0c1.153 0 2.242.078 3.324.214m9.236-1.003H4.827a2.25 2.25 0 0 0-2.231 2.079l.162 1.947m5.569 0H9.26" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12.56 0c.342.052.682.107 1.022.166m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
   </svg>
 );
 
-export const IconAI = IconSuru;
-
-// Replaced <img> tag with an SVG for the Copy Icon
-export const IconClipboardDocumentList: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
-  <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    fill="none" 
-    viewBox="0 0 24 24" 
-    strokeWidth={1.5} 
-    stroke="currentColor" 
-    className={className}
-    aria-hidden="true"
-  >
-    <path 
-      strokeLinecap="round" 
-      strokeLinejoin="round" 
-      d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" 
-    />
-  </svg>
-);
-
-
-// Thumbs Up Icon (Outline) 
-export const IconThumbUp: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.5c.806 0 1.533-.426 2.002-1.125H18.75a2.25 2.25 0 012.25 2.25v.167c0 .904-.426 1.708-1.125 2.252H15.188c-.862 1.69-2.607 2.875-4.688 2.875C7.138 17.125 4.5 14.526 4.5 11.137V9.375c0-1.125.9-2.025 2.025-2.025H6.633z" />
-  </svg>
-);
-
-// Thumbs Up Icon (Solid) 
-export const IconThumbUpSolid: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
-    <path d="M7.493 18.75c-.425 0-.82-.236-.975-.632A7.48 7.48 0 016 15.375c0-1.75.5-3.375 1.375-4.75A7.48 7.48 0 015.25 6H6.75a2.25 2.25 0 012.25-2.25H13.5a2.25 2.25 0 012.25 2.25V15h1.5a2.25 2.25 0 012.25 2.25v1.5a2.25 2.25 0 01-2.25-2.25H16.5a2.25 2.25 0 01-2.25-2.25V15H7.493v3.75z" />
-  </svg>
-);
-
-// Thumbs Down Icon (Outline) 
-export const IconThumbDown: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M17.367 13.5c-.806 0-1.533.426-2.002 1.125H5.25a2.25 2.25 0 01-2.25-2.25v-.167c0-.904.426-1.708 1.125-2.252h2.812c.862-1.69 2.607-2.875 4.688-2.875C15.862 6.875 18.5 9.474 18.5 12.863v1.762c0 1.125-.9 2.025-2.025 2.025h-.108z" />
-  </svg>
-);
-
-// Thumbs Down Icon (Solid) 
-export const IconThumbDownSolid: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
-     <path d="M16.507 5.25c.425 0 .82.236.975.632A7.48 7.48 0 0118 8.625c0 1.75-.5 3.375-1.375 4.75a7.48 7.48 0 012.125 4.75H17.25a2.25 2.25 0 01-2.25 2.25H9a2.25 2.25 0 01-2.25-2.25V9H5.25a2.25 2.25 0 01-2.25-2.25V5.25A2.25 2.25 0 015.25 3H7.5a2.25 2.25 0 012.25 2.25V9h6.757V5.25z" />
-  </svg>
-);
-
-// Retry Icon 
-export const IconArrowPath: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+export const IconClipboardDocumentList: React.FC<{ className?: string }> = ({ className = "w-5 h-5" }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className={className} aria-hidden="true">
+    <rect width="24" height="24" />
   </svg>
 );
