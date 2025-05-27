36278b893dae43de8000ceaaffdc0ac26ed75edb
surryaansh <suryanshs1804@gmail.com>
2025-05-27 16:23:24 +0530

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -122,30 +122,16 @@ export const IconAI = IconSuru;
 export const IconClipboardDocumentList: React.FC<{ className?: string }> = ({ className = "w-4 h-4" }) => (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
-    fill="none"
-    viewBox="0 0 24 24" 
-    strokeWidth="2.5"
-    stroke="currentColor" 
-    className={className}
+    width="16" 
+    height="16" 
+    fill="currentColor" 
+    className={className} // Apply the passed className here
+    viewBox="0 0 16 16"
     aria-hidden="true"
-    strokeLinecap="round" 
-    strokeLinejoin="round" 
   >
-    {/* Background square (appears top-right) - Drawn first */}
-    <rect 
-      x="8" 
-      y="4" 
-      width="12" 
-      height="12" 
-      rx="2.5"
-    />
-    {/* Foreground square (appears bottom-left) - Drawn second */}
-    <rect 
-      x="4" 
-      y="8" 
-      width="12" 
-      height="12" 
-      rx="2.5"
+    <path 
+      fillRule="evenodd" 
+      d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
     />
   </svg>
 );
