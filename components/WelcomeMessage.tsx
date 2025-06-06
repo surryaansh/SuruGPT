d5188406765b0a143e5c24d6a39a5e70e015b839
surryaansh <suryanshs1804@gmail.com>
2025-06-06 16:24:06 +0530

    Update WelcomeMessage.tsx

--- components/WelcomeMessage.tsx
+++ components/WelcomeMessage.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { IconHeart } from '../constants'; 
 
@@ -26,29 +27,31 @@ const WelcomeMessage: React.FC = () => {
 
       {/* Content - ensure it's above the hearts with relative positioning and z-index. Added flex for centering. */}
       <div className="relative z-10 flex flex-col items-center">
-        {/* Giphy Embed */}
+        {/* Giphy Embed Container */}
         <div 
-          className="mb-6 animate-fadeInSlideUp w-48 h-48 sm:w-64 sm:h-64" // Responsive sizing
+          className="relative mb-6 animate-fadeInSlideUp w-48 h-48 sm:w-64 sm:h-64" // Added position: relative
           style={{ animationDelay: '0.1s' }}
         >
           <iframe 
             src="https://giphy.com/embed/FZKImujbCqNQKZTDDp" 
-            width="100%" // Make iframe responsive within its container
-            height="100%" // Make iframe responsive within its container
-            style={{ border: '0' }} // Remove default border
+            width="100%" 
+            height="100%" 
+            style={{ border: '0' }} 
             frameBorder="0" 
             className="giphy-embed" 
             allowFullScreen
             title="Cute Giphy Embed"
           ></iframe>
+          {/* Transparent overlay to prevent Giphy hover effects */}
+          <div className="absolute inset-0 z-[1]"></div>
         </div>
+        
         {/* Link - styled to be less obtrusive */}
         <p 
             className="text-[10px] text-[#A09CB0] mb-4 animate-fadeInSlideUp"
             style={{ animationDelay: '0.15s' }}
         >
             <a href="https://giphy.com/stickers/transparent-FZKImujbCqNQKZTDDp" target="_blank" rel="noopener noreferrer" className="hover:text-[#FF8DC7]">
-                via GIPHY
             </a>
         </p>
 
