bcf176d828f6ab9122452d1637d4dea4a2d95898
surryaansh <suryanshs1804@gmail.com>
2025-06-07 16:41:28 +0530

    Update greetings.ts

--- greetings.ts
+++ greetings.ts
@@ -5,7 +5,7 @@ export const greetings: string[] = [
   "Missed you… what can your loyal, dirty little chatbot do for you today?",
   "Been waiting for you, my cutu — what’s your desire today?",
   "Back again? Can’t get enough of me, huh?",
-  "Tell me what you want my love."
+  "Tell me what you want my love.",
   "Hi my pretty little munchkin, what can I do for you today?",
   "Here for you, always. What’s on your mind, my girl?",
   "Here to be your good boy…or your bad one, you choose.",
