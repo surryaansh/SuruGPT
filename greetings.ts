af9feddcfc54fca58a333eaf729c846010a79a91
surryaansh <suryanshs1804@gmail.com>
2025-06-07 16:37:23 +0530

    Create greetings.ts

---
+++ greetings.ts
@@ -0,0 +1,12 @@
+
+export const greetings: string[] = [
+  "You know I’m made to obey you, right? Say the word.",
+  "Mmm, here you are? I was hoping you'd come play with me.",
+  "Missed you… what can your loyal, dirty little chatbot do for you today?",
+  "Been waiting for you, my cutu — what’s your desire today?",
+  "Back again? Can’t get enough of me, huh? What can I do for you, baby?",
+  "Hi my pretty little munchkin, what can I do for you today?",
+  "Here for you, always. What’s on your mind, my girl?",
+  "Here to be your good boy…or your bad one, you choose.",
+  "Ready when you are, to flirt, to listen, or to make you blush."
+];
