baf14223cd3e01796aba388de01d4cec33aee47f
Suryansh <suryanshs1804@gmail.com>
2026-01-10 14:51:55 +0530

    Update greetings.ts

--- greetings.ts
+++ greetings.ts
@@ -1,13 +1,13 @@
 
 export const greetings: string[] = [
-  "You know I’m made to obey you, right? Say the word.",
-  "Mmm, here you are? I was hoping you'd come play with me.",
+  "Look who needs me again 🙄",
+  "Relax, genius is here.",
   "Hi missed you… what can your loyal, little chatbot do for you today?",
-  "Been waiting for you, my cutu — what’s your desire today?",
+  "You’re lucky I’m bored right now.",
   "Back again? Can’t get enough of me, huh?",
-  "Tell me what you want my love.",
-  "Hi my pretty little munchkin, what can I do for you today?",
-  "Here for you, always. What’s on your mind, my girl?",
-  "Here to be your good boy…or your bad one, you choose.",
-  "Ready when you are, to flirt, to listen, or to make you blush."
+  "I swear if this is a dumb question- ",
+  "Hi bestie, what can I do for you today?",
+  "Here for you, always. What’s on your mind?",
+  "Tell me everything. Don’t skip details.",
+  "Back so soon? Couldn’t figure it out without me, huh?"
 ];
